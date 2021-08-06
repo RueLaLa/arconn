a81cab72657e2ed57671918a59561901766de60b
Nick Silverman <nsilverman@ruelala.com>
2021-08-06 11:54:57 -0400

    move cli def to main to fix odd behaviors

--- cmd/ec2-connect/main.go
+++ cmd/ec2-connect/main.go
@@ -2,27 +2,68 @@ package main
 
 import (
 	"fmt"
-	"github.com/ruelala/ec2-connect/lib"
+	"os"
+	"runtime"
+
 	"github.com/ruelala/ec2-connect/lib/awsClients"
 	"github.com/ruelala/ec2-connect/lib/awsClients/ec2"
 	"github.com/ruelala/ec2-connect/lib/awsClients/ssm"
+	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	args := lib.SetupApp()
+// these get passed in as ldflags by goreleaser
+var version string
+var commit string
+var date string
 
-	ttype := ec2.TargetType(args.Target)
+func print_version() string {
+	go_version := runtime.Version()
+	return fmt.Sprintf("ec2-connect %s built with %s on commit %s at %s", version, go_version, commit, date)
+}
+
+func main() {
+	app := &cli.App{
+		Name:    "ec2-connect",
+		Version: print_version(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "profile",
+				Aliases: []string{"p"},
+				EnvVars: []string{"AWS_PROFILE"},
+				Usage:   "aws profile for account specification",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "target",
+				Aliases:  []string{"t"},
+				Usage:    "target to start ssm session for",
+				Required: true,
+			},
+		},
+		Action: run,
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Printf("\n %s", err)
+		os.Exit(1)
+	}
+}
+
+func run(c *cli.Context) error {
+	ttype := ec2.TargetType(c.String("target"))
 	fmt.Println(fmt.Sprintf("input target type: %s", ttype))
 
-	aws_config := awsClients.AwsConfig(args.Profile)
+	aws_config := awsClients.AwsConfig(c.String("profile"))
 
 	id := ""
 	if ttype != "ID" {
-		id = ec2.Lookup(aws_config, args.Target, ttype)
+		id = ec2.Lookup(aws_config, c.String("target"), ttype)
 	} else {
-		id = args.Target
+		id = c.String("target")
 	}
 
 	ssm_client := ssm.Lookup(aws_config, id)
-	ssm.Connect(ssm_client, id, args.Profile)
+	ssm.Connect(ssm_client, id, c.String("profile"))
+	return nil
 }
