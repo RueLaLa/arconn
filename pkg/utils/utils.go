ebcf82ce78ab007920f3497334b7cbbd3e93488c
Nick Silverman <nsilverman@ruelala.com>
2022-07-21 13:07:09 -0400

    remove print and test files

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -50,3 +50,10 @@ func regex_match(pattern, target string) bool {
 		return false
 	}
 }
+
+func ChunkBy[T any](items []T, chunkSize int) (chunks [][]T) {
+	for chunkSize < len(items) {
+		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
+	}
+	return append(chunks, items)
+}
