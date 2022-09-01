51739f66db1be7a4e0e6926f9874e78efbf3f586
Nick Silverman <nsilverman@ruelala.com>
2022-09-01 16:42:51 -0400

    enable filtering ssm instances by name when theyre found

--- pkg/awsClients/ssm/ssm.go
+++ pkg/awsClients/ssm/ssm.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/manifoldco/promptui"
 	"github.com/ruelala/arconn/pkg/awsClients"
 	"github.com/ruelala/arconn/pkg/session-manager-plugin/sessionmanagerplugin/session"
 	_ "github.com/ruelala/arconn/pkg/session-manager-plugin/sessionmanagerplugin/session/portsession"
@@ -19,8 +20,61 @@ import (
 func Lookup(profile, target string, use_filter bool) string {
 	client := awsClients.SSMClient(profile)
 	resp := lookup_instance_in_ssm(client, target, use_filter)
+	instance := ""
+	if use_filter {
+		instance = *resp[0].InstanceId
+	} else {
+		instance = filter_matches(resp, target)
+	}
 	instance_online(resp, target)
-	return *resp[0].InstanceId
+	return instance
+}
+
+type Instance struct {
+	Name, ID string
+}
+
+func filter_matches(instances []types.InstanceInformation, target string) string {
+	var matches []Instance
+	for _, instance := range instances {
+		if *instance.Name == target {
+			matches = append(matches, Instance{
+				Name: *instance.Name,
+				ID:   *instance.InstanceId,
+			})
+		}
+	}
+
+	if len(matches) == 0 {
+		fmt.Println(fmt.Sprintf("no matching SSM instances found for %s", target))
+		return ""
+	} else if len(matches) == 1 {
+		fmt.Println(fmt.Sprintf("found %s currently running in SSM", matches[0].ID))
+		return matches[0].ID
+	} else {
+		instance_id := prompt_for_choice(matches)
+		return instance_id
+	}
+}
+
+func prompt_for_choice(instances []Instance) string {
+	templates := &promptui.SelectTemplates{
+		Active:   "\U00002713 {{ .Name | green }} {{ .ID | blue }}",
+		Inactive: "  {{ .Name | green }} {{ .ID | blue }}",
+		Selected: "\U00002713 {{ .ID | green }}",
+	}
+
+	prompt := promptui.Select{
+		Label:     "Select Instance to connect to",
+		Items:     instances,
+		Size:      10,
+		Templates: templates,
+	}
+
+	i, _, err := prompt.Run()
+	utils.Panic(err)
+
+	return instances[i].ID
 }
 
 func lookup_instance_in_ssm(client *ssm.Client, target string, use_filter bool) []types.InstanceInformation {
