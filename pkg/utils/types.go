c61100039abfa40d26d78dbad7fbc75a94f9ad8f
Nick Silverman <nsilverman@ruelala.com>
2023-01-10 14:44:17 -0500

    add remote host port forwarding, update dependencies, revert target argument to flag, and clean up default arg values

--- pkg/utils/types.go
+++ pkg/utils/types.go
@@ -1,11 +1,11 @@
 package utils
 
 type Args struct {
-	Profile, Target, Command, PortForward string
+	Profile, Target, Command, PortForward, RemoteHost string
 }
 
 type Target struct {
-	ResolvedName, SessionInfo, Type string
-	Resolved                        bool
-	PortForwarding                  []string
+	ResolvedName, SessionInfo, Type, RemoteHost string
+	Resolved                                    bool
+	PortForwarding                              []string
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -15,17 +15,19 @@ func Panic(err error) {
 }
 
 func ParseFlags(version_string string) Args {
-	args := Args{}
+	args := Args{
+		Command:     "/bin/bash",
+		Profile:     os.Getenv("AWS_PROFILE"),
+		PortForward: "",
+		RemoteHost:  "",
+	}
 
-	args.Profile = os.Getenv("AWS_PROFILE")
 	flaggy.String(&args.Profile, "p", "profile", "aws profile to use")
-
-	args.Command = "/bin/bash"
+	flaggy.String(&args.Target, "t", "target", "name of target")
 	flaggy.String(&args.Command, "c", "command", "command to pass to ecs targets")
+	flaggy.String(&args.PortForward, "P", "port-forward", "port forward map to use with ec2 targets (syntax 80 or 80:80 local:remote)")
+	flaggy.String(&args.RemoteHost, "r", "remote-host", "remote host to port forward to")
 
-	flaggy.String(&args.PortForward, "P", "portforward", "port forward map to use with ec2 targets (syntax 80 or 80:80 local:remote)")
-
-	flaggy.AddPositionalValue(&args.Target, "target", 1, true, "name of target")
 	flaggy.SetVersion(version_string)
 	flaggy.Parse()
 	return args
