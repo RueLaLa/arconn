fee3d83600aa74ce3ac3fd93c67e830f1396ee95
Nick Silverman <nsilverman@ruelala.com>
2023-03-03 14:05:28 -0500

    FINALLY figure out how to move the ldflags into non-main pkg's

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 	"os"
 	"os/user"
+	"runtime"
 
 	"github.com/integrii/flaggy"
 )
+
+// these get passed in as ldflags by goreleaser
+var version string
+var commit string
+var date string
+
+func print_version() string {
+	go_version := runtime.Version()
+	return fmt.Sprintf("arconn %s built with %s on commit %s at %s", version, go_version, commit, date)
+}
 
 func Panic(err error) {
 	if err != nil {
@@ -15,15 +26,15 @@ func Panic(err error) {
 	}
 }
 
-func GetUser() string {
+func GetSessionName() string {
 	user, err := user.Current()
 	if err != nil {
-		return "unknown user"
+		return fmt.Sprintf("arconn-%s-unknown-user", version)
 	}
-	return user.Username
+	return fmt.Sprintf("arconn-%s-%s", version, user.Username)
 }
 
-func ParseFlags(version_string string) Args {
+func ParseFlags() Args {
 	args := Args{
 		Command:     "/bin/bash",
 		Profile:     os.Getenv("AWS_PROFILE"),
@@ -37,7 +48,7 @@ func ParseFlags(version_string string) Args {
 	flaggy.String(&args.PortForward, "P", "port-forward", "port forward map to use with ec2 targets (syntax 80 or 80:80 local:remote)")
 	flaggy.String(&args.RemoteHost, "r", "remote-host", "remote host to port forward to")
 
-	flaggy.SetVersion(version_string)
+	flaggy.SetVersion(print_version())
 	flaggy.Parse()
 	return args
 }
