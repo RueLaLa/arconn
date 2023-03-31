ece7584b15b487c2b3abe6624270ddcc1cafb349
Nick Silverman <nsilverman@ruelala.com>
2023-03-31 17:03:39 -0400

    also support older ssm versions that cant multiplex

--- pkg/session-manager-plugin/sessionmanagerplugin/session/session.go
+++ pkg/session-manager-plugin/sessionmanagerplugin/session/session.go
@@ -203,11 +203,10 @@ func ValidateInputAndStartSession(args []string, out io.Writer) {
 	}
 
 	if err = startSession(&session, log); err != nil {
-		if session.DataChannel.IsSessionEnded() == true {
-			return
+		if session.DataChannel.IsSessionEnded() == false {
+			log.Errorf("Cannot perform start session: %v", err)
+			fmt.Fprintf(out, "Cannot perform start session: %v\n", err)
 		}
-		log.Errorf("Cannot perform start session: %v", err)
-		fmt.Fprintf(out, "Cannot perform start session: %v\n", err)
 		return
 	}
 }
@@ -234,7 +233,9 @@ func (s *Session) Execute(log log.T) (err error) {
 		s.SessionType = s.DataChannel.GetSessionType()
 		s.SessionProperties = s.DataChannel.GetSessionProperties()
 		if err = setSessionHandlersWithSessionType(s, log); err != nil {
-			log.Errorf("Session ending with error: %v", err)
+			if s.DataChannel.IsSessionEnded() == false {
+				log.Errorf("Session ending with error: %v", err)
+			}
 			return
 		}
 	}
